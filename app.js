4a54c61d5313cb091f0a7532daec231bfd704687
Robert Ward <raward4@ncsu.edu>
2022-01-29 22:20:30 -0500

    js-objects-lab

--- app.js
+++ app.js
@@ -3,11 +3,21 @@ const game = {
   biggestNum: 100,
   smallestNum: 1,
   secretNum: null,
+  prevGuesses: [],
+  getGuess: function() {
+    return ("Please enter a guess between " + this.smallestNum + " and " + this.biggestNum)},
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
-      (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+      (this.biggestNum - this.smallestNum + 1)) + this.smallestNum;
+    console.log(getGuess()); 
   }
+ 
 }
-console.log(prevGuesses.length)
+
+
+  
+
 console.log(game)
+
+
 
